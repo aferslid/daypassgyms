b9b5f29fdf0f7e414626e381bf18a1e9c0609f74
aferslid <aferslidou@gmail.com>
2026-06-18 19:16:34 +0200

    debug env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("KEY =", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10));
+console.log("SUPABASE URL", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE KEY", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10));
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
